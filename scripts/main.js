c81499569e97634ae954d74ed84c30b0d89dfd6a
Daniel Nguyen <danielnguyen93@hotmail.com>
2016-12-21 19:08:49 -0800

    added new photo and javascript function

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images-folder/appicns_firefox.png') {
+      myImage.setAttribute ('src','images-folder/mozilla-firefox-icon-4760.png');
+    } else {
+      myImage.setAttribute ('src','images-folder/appicns_firefox.png');
+    }
+}
